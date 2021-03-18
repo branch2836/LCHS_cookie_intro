056f6b3c0f810ce0e2f6cdf65fc0c0b346bb1d9b
jimflores <jflores@launchcode.org>
2021-03-18 12:42:49 -0500

    Established starter code for the Flask cookie video.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, render_template, make_response
+import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
+    cookie_name = 'oatmeal raisin'
+    number = random.randint(1, 50)
 
-   return render_template('index.html')
+    return render_template('index.html', number = number)
 
 if __name__ == '__main__':
-   app.run()
+    app.run()
