54ce7d5ac90a06c7f5a73cc2434b3d9772df3c72
jimflores <jflores@launchcode.org>
2021-03-18 14:40:01 -0500

    Added content to the solution code.

--- main.py
+++ main.py
@@ -6,10 +6,20 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    cookie_name = 'oatmeal raisin'
-    number = random.randint(1, 50)
+    number = random.randint(1, 50)  # Select a random number from 1 - 50.
 
-    return render_template('index.html', number = number)
+    # make_response() builds the HTTP response that Flask sends to our browser.
+    # In this case, the HTML code from index.html is used as the argument.
+    http_response = make_response(render_template('index.html', number = number))
+
+    cookie_flavor = 'thin mints'  # This string will become the value for the cookie.
+    
+    # Use the .set_cookie() method to define the name and value for a new cookie.
+    http_response.set_cookie('fav_cookie', cookie_flavor)
+    # This key/value pair will be sent in the HTTP response and stored as a file on
+    # our device.
+
+    return http_response
 
 if __name__ == '__main__':
     app.run()
