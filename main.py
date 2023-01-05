7b0a9f1f3abe606a609925456adca9bd16c6d293
Branch, Christopher <Christopher_Branch@comcast.com>
2023-01-05 14:26:19 -0500

    final 25.4

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, session
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = "this_is_not_a_good_key"
 
 @app.route('/')
 def index():
     number = random.randint(1, 50)
 
-    return render_template('index.html', number = number)
+    http_response = make_response(render_template('index.html', number = number))
+    cookie_flavor="Chocolate Chip"
+    http_response.set_cookie("Favorite Cooke", cookie_flavor)
+    return http_response
+
 
 if __name__ == '__main__':
     app.run()
